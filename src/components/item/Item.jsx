eeb6105f7043a248f963ffd39800266009eaf164
yakupkose3123 <yakupkose3123@gmail.com>
2022-04-01 00:23:48 +0300

    update item.jsx

--- src/components/item/Item.jsx
+++ src/components/item/Item.jsx
@@ -1,16 +1,32 @@
-
+import { useState } from "react"
+import "./Item.css"
 
 
 const Item = (props) => {
     const{name,img,options} = props.card
+
+    const [showLogo, setshowLogo] = useState(true)
+
+
+    const showAnswer = () => setshowLogo(!showLogo)   // her seferinde tersine çevir kardeş 
+    
+
   return (
       
-    < div className="card">
+    < div className="card" onClick={showAnswer}>
+      { showLogo ?
+        (<div className="front">
         <img src={img} alt="" className="card-img"/>
-        <h5 className="card-title">{name}</h5>     
-           
-    
-      
+        <h5 className="card-title">{name}</h5> 
+      </div>)
+        
+          :
+
+      (<div className="back" >
+        <ul>
+          {options.map((option)=> <li>{option}</li>)}
+        </ul>
+      </div> ) }      
     </div>
   )
 }
